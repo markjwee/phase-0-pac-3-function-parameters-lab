5f161e23f241afd01de937c800fd3266cafdab91
Mark <mark.j.wee@gmail.com>
2021-12-01 20:30:47 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -2,27 +2,10 @@ function introduction(firstName) {
     return `Hi, my name is ${firstName}.`
 }
 
-
-function introductionWithLanguage(firstName, programmingLanguage) {
-    var Josh = "Hi, my name is Josh and I am learning to program in Ember.js";
-    var Nancy = "Hi, my name is Nancy and I am learning to program in React.";
-    if (firstName === Josh){
-        return Josh;
-    }
-    else if (firstName === "Nancy"){
-        return Nancy;
-    }
-    else if (firstName()){
-        return Josh;
-    }
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 
-// function introductionWithLanguageOptional(firstName = "User") {
-//     var firstName = 
-//     var programmingLanguage = false;
-//     introductionWithLanguageOptional ? "Hello, my name is ${firstName}!" : "Hello, my name is ${firstName}, and I'm learning to program ${programmingLanguage}";
-//     console.log(`Hello, my name is ${firstName}!`);
-// } // `Hello, my name is ${firstName}, and I am learning to program in ${programmingLanguage}`
-
-
-
+function introductionWithLanguageOptional(name, language="JavaScript") {
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
